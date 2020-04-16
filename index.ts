f194342f290751fbb8ff909300dedefb054476db
tpluscode <tpluscode@users.noreply.github.com>
2020-04-16 21:31:38 +0200

    writing nicer turtle

--- index.ts
+++ index.ts
@@ -1,28 +1,29 @@
 import walk from '@fcostarodrigo/walk'
 import cf = require('clownface')
 import $rdf = require('rdf-ext')
-import { toFile } from 'rdf-utils-fs'
 import fs = require('fs-extra')
 import { namedNode } from '@rdfjs/data-model'
+import {turtle} from '@tpluscode/rdf-string';
 
 require = require("esm")(module)
 
+const base = process.env.URL || 'http://localhost:8080'
+
 async function main() {
     for await (const file of walk('src')) {
-        const term = file.replace(/\.ts$/, '')
+        const term = file.replace(/^src\/(.+)\.ts$/, '$1')
         const outFile = file
             .replace(/^src\/(.+)\.ts$/, 'dist/$1.ttl')
 
         const graph = cf({
             dataset: $rdf.dataset(),
-            term: namedNode(term)
+            term: namedNode(`${base}/${term}`)
         })
 
         const { build } = await import((`./${file}`))
         await build(graph)
 
-        await fs.ensureFile(outFile)
-        await toFile(graph.dataset.toStream(), outFile)
+        await fs.outputFile(outFile, turtle`${graph.dataset}`.toString({ base }))
     }
 }
 
