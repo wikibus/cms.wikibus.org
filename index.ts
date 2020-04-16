22afa0f8c8e3ea9ac06bc209a9815a0f7f3b347b
tpluscode <tpluscode@users.noreply.github.com>
2020-04-16 21:17:57 +0200

    feat: first attempt

---
+++ index.ts
@@ -0,0 +1,32 @@
+import walk from '@fcostarodrigo/walk'
+import cf = require('clownface')
+import $rdf = require('rdf-ext')
+import { toFile } from 'rdf-utils-fs'
+import fs = require('fs-extra')
+import { namedNode } from '@rdfjs/data-model'
+
+require = require("esm")(module)
+
+async function main() {
+    for await (const file of walk('src')) {
+        const term = file.replace(/\.ts$/, '')
+        const outFile = file
+            .replace(/^src\/(.+)\.ts$/, 'docs/$1.ttl')
+
+        const graph = cf({
+            dataset: $rdf.dataset(),
+            term: namedNode(term)
+        })
+
+        const { build } = await import((`./${file}`))
+        await build(graph)
+
+        await fs.ensureFile(outFile)
+        await toFile(graph.dataset.toStream(), outFile)
+    }
+}
+
+main().then(() => process.exit()).catch(e => {
+    console.error(e)
+    process.exit(1)
+})
---
+++ src/contributing/brochures.ts
@@ -0,0 +1,5 @@
+import HowTo from '@rdfine/schema/HowTo'
+
+export const build = async (arg) => new HowTo.Class(arg, {
+    name: "Contributing brochures to wikibus.org"
+})
